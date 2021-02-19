85d1b235f6d7700169a6a25f88f2bdeda611ddc2
avgra3 <antonygradillas@gmail.com>
2021-02-18 18:13:40 -0700

    Profile images auto-resized if too large

--- website/users/models.py
+++ website/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 # Class that inherits from the Model from Django
@@ -11,3 +12,14 @@ class Profile(models.Model):
     # This is how we want the Profile to be displayed
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        # resizes profile pics to smaller sizes
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
