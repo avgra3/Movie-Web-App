4e95dfa144c02143b128e5efb6de4ea5a90d4f3d
avgra3 <antonygradillas@gmail.com>
2021-02-21 15:51:52 -0700

    login reset completed

--- website/website/settings.py
+++ website/website/settings.py
@@ -130,7 +130,7 @@ LOGIN_URL = 'login'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
+# Sending Email to user to reset their passwords
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
